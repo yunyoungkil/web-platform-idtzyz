cc27c7654c07ba8b8445ffd2d825c5f2adcbd25a
yunyoungkil <yunyoungkil76@gmail.com>
2022-03-15 22:43:24 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const myObj = {client_id: "John", client_secret: 31, redirect_uri: "New York" , code: "New York", grant_type: "New York"};
+
+json_obj
+xhttp.open("GET", "https://www.tistory.com/oauth/access_token?client_id={}", true);
+xhttp.send();
